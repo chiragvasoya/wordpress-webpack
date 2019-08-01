b77146819098d54533286346a934e4793255f79c
chiragvasoya <vasoyachirag@live.com>
2019-08-01 14:18:50 -0600

    Changed style and script methods name for future theme

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ function ilc_custom_login() {
 
 
 
-function register_ledworld_menus(){
+function register_themename_menus(){
 	register_nav_menus(
 		array(
 		'main_nav' => __('Main Navigation'), 
@@ -24,24 +24,24 @@ function register_ledworld_menus(){
 		'footer_nav' => __('Footer Navigation'))
 		 );
 }
-add_action( 'init', 'register_ledworld_menus');
+add_action( 'init', 'register_themename_menus');
 
 
 
 
 /*************************     Styles and JavaScript files  *********************/
-function ledworldus_enqueue_styles() {
+function themename_enqueue_styles() {
     wp_enqueue_style( 'bundlecss', get_stylesheet_directory_uri().'/dist/bundle.css'); 
 }
-add_action( 'wp_enqueue_scripts', 'ledworldus_enqueue_styles' ); 
+add_action( 'wp_enqueue_scripts', 'themename_enqueue_styles' ); 
 
-function ledworldus_enqueue_scripts() {
+function themename_enqueue_scripts() {
     $dependencies = array('jquery');
     wp_enqueue_script('bundlejs', get_template_directory_uri().'/dist/bundle.js', $dependencies, '', true );
 }
  
 
-add_action( 'wp_enqueue_scripts', 'ledworldus_enqueue_scripts' );
+add_action( 'wp_enqueue_scripts', 'themename_enqueue_scripts' );
 
 
 
@@ -61,44 +61,6 @@ if(function_exists('add_image_size')){
 }
 
 
-
-/*******************     Slider Custom Post Type     *************************************/
-add_action('init', 'led_slider_register');
- 
-function led_slider_register() {
- 
-	$labels = array(
-		'name' => _x('LED Slider', 'post type general name'),
-		'singular_name' => _x('Slider Item', 'post type singular name'),
-		'add_new' => _x('Add New', 'Slide item'),
-		'add_new_item' => __('Add New Slide Item'),
-		'edit_item' => __('Edit Slide Item'),
-		'new_item' => __('New Slide Item'),
-		'view_item' => __('View Slide Item'),
-		'search_items' => __('Search Slider'),
-		'not_found' =>  __('Nothing found'),
-		'not_found_in_trash' => __('Nothing found in Trash'),
-		'parent_item_colon' => ''
-	);
- 
-	$args = array(
-		'labels' => $labels,
-		'public' => true,
-		'publicly_queryable' => true,
-		'show_ui' => true,
-		'query_var' => true,
-		'menu_icon' => get_stylesheet_directory_uri() . '/src/images/slider.png',
-		'rewrite' => true,
-		'capability_type' => 'post',
-		'hierarchical' => false,
-		'menu_position' => null,
-		'taxonomies' => array('category', 'post_tag'),
-		'has_archive' => true,
-		'supports' => array('title','editor','thumbnail','custom-fields')
-	  ); 
- 
-	register_post_type( 'LED Slider' , $args );
-}
 
 
 
